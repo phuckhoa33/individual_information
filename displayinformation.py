6a14c9ad9c04f7f4b9f2cef1aaf1ad27e6c64531
Nguyễn Khoa Minh Phúc <94945244+33phuckhoa@users.noreply.github.com>
2022-11-03 08:53:48 +0700

    Add a new file

---
+++ displayinformation.py
@@ -0,0 +1,4 @@
+print("Name: Nguyen Khoa Minh Phuc\n"+
+        "Born: 03/03/2003\n"+
+        "Address: E13/16/J8 Group 18, Hamlet 5A, Vinh Loc A Commune, Binh Chanh District, HCM city\n"+
+        "My goals: Learn python so that prepare a tool for my data science journey")
